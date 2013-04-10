6921eec1f11f3b1bf59f8b1ac927803c933a657f
ilvalle <paolo.valleri@gmail.com>
2013-04-10 11:44:12 +0200

    fix data ordered, updated analytics

--- controllers/plot.py
+++ controllers/plot.py
@@ -10,21 +10,20 @@ def get_history():
 	#n_days = int(request.vars.interval) if request.vars.interval and request.vars.interval.isdigit() else 7
 	n_hours = int(request.vars.interval) if request.vars.interval and request.vars.interval.isdigit() else 1
 	if not(station_id and station_id.isdigit()): raise HTTP(404)
-	now = request.now
+	#now = request.now
 	#days_back = now - timedelta(days=n_days)
 	epoch = db.record.gathered_on.epoch()
+	day = db.record.gathered_on.day()
 	import time
 	t0 = time.time()	
 	data = db(	(db.record.station_id == station_id) 
 				#& (db.record.gathered_on >= days_back) 
 				#& (db.record.gathered_on <= request.now)
-			).select(epoch, cache=(cache.memcache, 3600))
+			).select(epoch, orderby=epoch, cache=(cache.ram, 3600))
 	t1 = time.time()
 	output = []
-	for key, group in groupby(data, lambda x: x[epoch] / ( 60 * 60 * n_hours)):
+	for key, group in groupby(data, lambda x: (x[epoch] + 3600) / ( 60 * 60 * n_hours)):
 		l = list(group)
-		for l in group:
-			print l[epoch]
 		output.append( [ key*60*60*1000*n_hours, len(l)] ) 
 	t2 = time.time()
 	#print 'T', t2-t0, 't0', t1-t0, 't1', t2-t1
