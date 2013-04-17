c73bd74ad01222ed2b0eaec8befd3f11390141d2
ilvalle <paolo.valleri@gmail.com>
2013-04-17 16:59:39 +0200

    Minor css fix

--- models/utils.py
+++ models/utils.py
@@ -1,6 +1,6 @@
 openstreatmap_static_link="http://ojw.dev.openstreetmap.org/StaticMap/?lat=%(lat)s&lon=%(lon)s&mlat0=%(lat)s&att=none&mlon0=%(lon)s&z=%(zoom)s&h=%(height)s&w=%(width)s&layer=mapnik&mode=Export&show=1"
 
-def get_static_img(lat, lon, width='150', height='150', zoom=15):
+def get_static_img(lat, lon, width='170', height='170', zoom=14):
 	if not(lat and lon): return ''
 	url = openstreatmap_static_link % {'lon':lon, 'lat':lat, 'height':150, 'width':width, 'zoom':zoom}
-	return str(IMG(_src=url))
+	return str(IMG(_src=url, _title="map"))
