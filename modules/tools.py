f1de211c05bbee0df5daa489b25d0a979d1e725a
ilvalle <paolo.valleri@gmail.com>
2013-06-12 11:18:52 +0200

    fix pytho 2.6 compatibility

--- modules/tools.py
+++ modules/tools.py
@@ -5,4 +5,5 @@ EPOCH_REF = datetime.datetime(1970, 1, 1, 0, 0, tzinfo = pytz.utc)
 def EPOCH_M(current_datetime):
 	utc_datetime = pytz.utc.localize(current_datetime) # fix use rome and store data in utc instead of rome timezone
 	delta = utc_datetime - EPOCH_REF
-	return int(delta.total_seconds())
+	seconds = delta.seconds + (delta.days * 24 * 3600)
+	return seconds
