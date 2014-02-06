5ac8eaa7d208277710d59aa776cedf2defdbd3f8
ilvalle <paolo.valleri@gmail.com>
2014-02-06 16:09:39 +0100

    hardcoded frametime

--- controllers/console.py
+++ controllers/console.py
@@ -6,7 +6,7 @@ import requests
 # load html as it is done in web2py.js
 # 
 #response.headers['web2py-component-flash'] = ''
-seconds = int(datetime.timedelta(days=requested_period).total_seconds())
+seconds = int(datetime.timedelta(seconds=1800).total_seconds())
 # temp fix due to double menu
 zero = request.args(0) or 'index'
 if request.function != 'wiki' and zero and not(zero.isdigit()):
@@ -16,7 +16,7 @@ baseurl = "http://ipchannels-test.integreen-life.bz.it"
 # ipchannels-test.integreen-life.bz.it/MeteoFrontEnd/get-records?station=8320&name=WG&unit=m/s&seconds=3000
  
 def index():
-	frontends = ['MeteoFrontEnd']
+	frontends = ['MeteoFrontEnd', 'VehicleFrontEnd']
 	return response.render('console/index.html', {'frontends':frontends, 'seconds':seconds})
 
 def get_stations():
@@ -46,7 +46,7 @@ def get_data_types():
     url = "%s/%s/rest/get-data-types" % (baseurl, frontend)
     r = requests.get(url, params={'station':station})
     data_types = r.json()
-    
+
     return response.render('console/data_types_legend.html', {'data_types':data_types, 'frontend':frontend, 'station':station })
 
 
@@ -65,10 +65,10 @@ def get_data():
 #    print 'l', data_label
     
     url = "%s/%s/rest/get-records" % (baseurl, frontend)
-    params = {'station':station, 'station':station, 'name':data_type, 'unit':unit, 'seconds':seconds}
+    params = {'station':station, 'name':data_type, 'unit':unit, 'seconds':seconds}
     r = requests.get(url, params=params)
     data = r.json()
-    output = [ [row['timestamp'], row['value']] for row in data ]
+    output = [ [row['timestamp'] if 'timestamp' in row else row['ts_ms'], row['value']] for row in data ]
     # the id must be the same of the A element in the data type list
     series = [{'data':output, 'id': 'type_%s_%s' % (station,data_type), 'station_id':'station_iud', 'label': "%s - %s" % (station, data_label)}]
     return response.json({'series': series})
