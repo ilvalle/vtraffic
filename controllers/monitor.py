5236b03a73e468ebb5fcbe9dfd701d5f1b610f85
ilvalle <paolo.valleri@gmail.com>
2014-05-28 15:00:08 +0200

    added method to check whether a box is still working or not (monitored with nagios)

---
+++ controllers/monitor.py
@@ -0,0 +1,13 @@
+import datetime
+from datetime import timedelta
+
+def box():
+    if not(request.vars.id) or not(request.vars.id.isdigit()): return 'specificare id (integer)'
+    station_id = int(request.vars.id)
+    time_delta = request.now - datetime.timedelta(days=2)
+    query = (db.record.station_id == station_id) & (db.record.gathered_on > time_delta)
+    empty = db(query).isempty()
+    if empty:
+        raise(HTTP(500, 'no logs'))
+    else:
+        return 'ok'        
