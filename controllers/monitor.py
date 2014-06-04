35a7a735a918ba6f577ed2428fb742a6aec99c68
ilvalle <paolo.valleri@gmail.com>
2014-06-04 10:33:48 +0200

    send an alert if no bluetooth were detected in the last 24h

--- controllers/monitor.py
+++ controllers/monitor.py
@@ -5,7 +5,7 @@ def box():
     session.forget()
     if not(request.vars.id) or not(request.vars.id.isdigit()): return 'specificare id (integer)'
     station_id = int(request.vars.id)
-    time_delta = request.now - datetime.timedelta(days=2)
+    time_delta = request.now - datetime.timedelta(days=1)
     query = (db.record.station_id == station_id) & (db.record.gathered_on > time_delta)
     empty = db(query).isempty()
     if empty:
