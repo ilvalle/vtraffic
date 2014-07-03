fab507b6ffb2bd04e5353f425974e3e36cfc8ee9
ilvalle <paolo.valleri@gmail.com>
2014-07-03 10:32:48 +0200

    store last check timestamp

--- controllers/monitor.py
+++ controllers/monitor.py
@@ -5,6 +5,10 @@ session.forget()
 def box():
     if not(request.vars.id) or not(request.vars.id.isdigit()): return 'specificare id (integer)'
     station_id = int(request.vars.id)
+    station = db(db.station.id == station_id).select(limitby=(0,1)).first()
+    if not(station):
+        raise(HTTP(500, "requested station doesn't exist"))
+    station.update_record(last_check_on=request.now)
     time_delta = request.now - datetime.timedelta(days=1)
     query = (db.record.station_id == station_id) & (db.record.gathered_on > time_delta)
     empty = db(query).isempty()
