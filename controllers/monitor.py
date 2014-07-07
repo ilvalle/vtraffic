389bc3d97073276268100b8195cf34867165a5e0
ilvalle <paolo.valleri@gmail.com>
2014-07-07 10:24:20 +0200

    monitoring api for Meteo and environment stations

--- controllers/monitor.py
+++ controllers/monitor.py
@@ -30,3 +30,26 @@ def situation():
         db.station.id.readable=False
     grid = SQLFORM.grid(db.station, csv=False, searchable=False)
     return {'grid':grid}
+    
+    
+def meteo():
+    db_intime.station._common_filter = lambda query: db_intime.station.stationtype == 'Meteostation'
+    return __check_measurementhistory('Meteo')
+
+def environment():
+    db_intime.station._common_filter = lambda query: db_intime.station.stationtype == 'Environmentstation'
+    return __check_measurementhistory('Environment')
+    
+# check for each type if the data is not older than 1day
+def __check_measurementhistory(name):
+    time_delta = request.now + datetime.timedelta(days=1)
+    max_ts = db_intime.measurementhistory.timestamp.max()
+    query = (db_intime.measurementhistory.station_id == db_intime.station.id) & \
+            (db_intime.measurementhistory.type_id == db_intime.type.id)
+    query_having = (max_ts < time_delta)            
+    id_list = db_intime(query).select(db_intime.measurementhistory.type_id,db_intime.measurementhistory.station_id, cacheable=True, 
+                                      groupby=db_intime.measurementhistory.station_id | db_intime.measurementhistory.type_id, having=query_having).as_list()
+    if len(id_list) != 0:
+        raise(HTTP(500, '%s logs are older than 1day, %s' % (name, id_list)))
+    else:
+        return 'ok'
