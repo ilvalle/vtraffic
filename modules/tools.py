aacca8f74ed5ac7e81069c72839940aa943d288e
ilvalle <paolo.valleri@gmail.com>
2014-08-21 17:45:04 +0200

    introduction of a new DAL (intimeDAL) with few useful methods

--- modules/tools.py
+++ modules/tools.py
@@ -2,6 +2,7 @@ import pytz
 import datetime
 import xmlrpclib
 import httplib
+from gluon.dal import DAL
 
 EPOCH_REF = datetime.datetime(1970, 1, 1, 0, 0, tzinfo = pytz.utc)
 
@@ -11,7 +12,7 @@ def EPOCH_M(current_datetime):
 	seconds = delta.seconds + (delta.days * 24 * 3600)
 	return seconds
 
-__all__ = ['TimeoutTransport']
+__all__ = ['TimeoutTransport', 'intimeDAL']
 
 class TimeoutTransport(xmlrpclib.Transport):
     timeout = 20.0
@@ -20,3 +21,40 @@ class TimeoutTransport(xmlrpclib.Transport):
     def make_connection(self, host):
         return httplib.HTTPConnection(host, timeout=self.timeout)
 
+
+class intimeDAL(DAL):
+    ## save everything in elaborationhistory
+    def save_elaborations(self, rows, station_id, type_id, interval, unique=True):
+        return self.__save_records(rows, station_id, type_id, interval, table='elaborationhistory', unique=unique)
+
+    ## save everything in measurementhistory
+    def save_measurements(self, rows, station_id, type_id, interval, unique=True):
+        return self.__save_records(rows, station_id, type_id, interval, table='measurementhistory', unique=unique)
+
+    ## save data in a general intime table
+    def __save_records(self, rows, station_id, type_id, interval, table, unique=True):
+        from datetime import timedelta
+        t = self[table]
+
+        for r in rows:
+            if interval != 1:
+                new_timestamp = r['timestamp'] + timedelta(seconds=interval/2)
+            else:
+                new_timestamp = r['timestamp']
+            values = {'created_on':datetime.datetime.now(),
+                      'timestamp': new_timestamp,
+                      'value': r['value'],
+                      'station_id': station_id,
+                      'type_id': type_id,
+                      'period': interval}
+            if unique:
+                t.update_or_insert(((t.timestamp == new_timestamp) &
+                                    (t.station_id == station_id) &
+                                    (t.type_id == type_id) &
+                                    (t.period == interval)),
+                                   **values)
+            else:
+                t.insert(**values)
+
+        self.commit()
+        return len(rows)
