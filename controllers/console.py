5802112a85abf94ec80849e1f4c82c563f56a185
ilvalle <paolo.valleri@gmail.com>
2015-02-02 19:23:48 +0100

    return 1 data every 10 for vehicleFE

--- controllers/console.py
+++ controllers/console.py
@@ -103,7 +103,7 @@ def get_data():
 
     if frontend == "Vehicle":
         #### Take 1 value every 5 or 10
-        output = [ [data[i]['timestamp'], "%.2f" % float(data[i]['value'])]  for i in xrange(0, len(data), 1)]
+        output = [ [data[i]['timestamp'], "%.2f" % float(data[i]['value'])]  for i in xrange(0, len(data), 10)]
     else:
         #### Average value every 5 seconds
         output = []
